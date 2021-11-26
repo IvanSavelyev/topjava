a5987f68053cad81c9b2d76d7c2ff0b423538d9a
admin340 <ivsound93@gmail.com>
2021-11-26 17:13:18 +0300

    Add MealRestController and MealRestControllerTest

---
+++ src/test/java/ru/javawebinar/topjava/web/meal/MealRestControllerTest.java
@@ -0,0 +1,32 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MealRestControllerTest {
+
+    @Test
+    void edit() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void testCreate() {
+    }
+
+    @Test
+    void filter() {
+    }
+}
